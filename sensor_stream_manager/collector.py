7f79c1ee6ceb2dba9320fbd20e7e34fea7b8d44a
svartfika <svartfika@proton.me>
2025-02-05 19:16:09 +0100

    feat: Add loading environment variables

--- sensor_stream_manager/collector.py
+++ sensor_stream_manager/collector.py
@@ -1,19 +1,26 @@
 from abc import ABC
 import requests
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 class Collector(ABC):
     pass
 
+
 class Phyphox(Collector):
-    phyphox_host = "0.0.0.0"
-    phyphox_port = 65535  # 49152-65535
+    phyphox_host = os.getenv("PHYPHOX_HOST")
+    phyphox_port = os.getenv("PHYPHOX_PORT")  # 49152-65535
     phyphox_endpoint = f"http://{phyphox_host}:{phyphox_port}"
 
     request_source = "linear_acceleration"
     request_buffers = {"accX", "accY", "accZ"}
 
-    experiment_config = requests.get(phyphox_endpoint + "/config").json()
-    
+    experiment_config = requests.get(phyphox_endpoint + "/config").json()  # try-block here
+
     experiment_sources = {input.get("source") for input in experiment_config.get("inputs")}
     if request_source not in experiment_sources:
         raise ValueError(f"Requierd source '{request_source}' not found in experiment")
