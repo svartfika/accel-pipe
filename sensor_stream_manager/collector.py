9f554a7a8ff9b9f4721517a2acb0d2cba081a539
svartfika <svartfika@proton.me>
2025-02-06 20:19:20 +0100

    feat: Add function for getting sensor data from api

--- sensor_stream_manager/collector.py
+++ sensor_stream_manager/collector.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from collections.abc import Generator
 import requests
 
 from dotenv import load_dotenv
@@ -8,7 +9,8 @@ load_dotenv()
 
 
 class Collector(ABC):
-    pass
+    def get_data(self) -> Generator[float | None]:
+        raise NotImplementedError
 
 
 class Phyphox(Collector):
@@ -39,24 +41,45 @@ class Phyphox(Collector):
         try:
             response = requests.head(self.phyphox_endpoint)
             response.raise_for_status()
+
         except (requests.RequestException, requests.Timeout):
             raise ConnectionError("Service unavailable")
 
     def _validate_config(self) -> None:
         try:
-            response_config = requests.get(self.phyphox_endpoint + "/config").json()
+            response_config = requests.get(self.phyphox_endpoint + "/config")
+            response_config.raise_for_status()
+            json_config = response_config.json()
 
-            experiment_sources = {input.get("source") for input in response_config.get("inputs")}
-            experiment_buffers = {buffer.get("name") for buffer in response_config.get("buffers")}
+            experiment_sources = {input.get("source") for input in json_config.get("inputs")}
+            experiment_buffers = {buffer.get("name") for buffer in json_config.get("buffers")}
 
             if self.request_source not in experiment_sources:
                 raise ValueError(f"Source not found: '{self.request_source}'")
 
             if not set(self.request_buffers) <= experiment_buffers:
                 raise KeyError(f"Buffers not found: {self.request_buffers - experiment_buffers}")
+
         except (requests.RequestException, requests.Timeout):
             raise ConnectionError("Config inaccessible")
 
+    def get_data(self) -> Generator[float | None]:
+        try:
+            response_data = requests.get(f"{self.phyphox_endpoint}/get?{'&'.join(self.request_buffers)}")
+            response_data.raise_for_status()
+            json_data = response_data.json()
+
+            return (
+                next(iter(json_data.get("buffer", {}).get(buffer, {}).get("buffer", [])), None)
+                for buffer in self.request_buffers
+            )
+
+        except (requests.RequestException, requests.Timeout):
+            raise ConnectionError("Service unavailable")
+
+        except requests.exceptions.JSONDecodeError as e:
+            raise ValueError(f"JSON Decode Error: {e}")
+
 
 if __name__ == "__main__":
-    Phyphox()
+    Phyphox().get_data()
