5b75df89d13a6a8815176fba91ab313f2128d14e
svartfika <svartfika@proton.me>
2025-02-14 18:48:53 +0100

    add: initial draft of async sensor manager with callbacks

---
+++ sensor_stream_manager/manager.py
@@ -0,0 +1,45 @@
+from collector import Collector, Phyphox
+import asyncio
+from contextlib import AsyncExitStack
+
+
+class SensorStream:
+    def __init__(self):
+        self.collectors: list[Collector] = []
+        self.callbacks: list[callable] = []
+        self.running = False
+
+    def add_collector(self, collector: Collector):
+        self.collectors.append(collector)
+
+    def add_callback(self, callback: callable):
+        self.callbacks.append(callback)
+
+    async def start(self):
+        self.running = True
+        async with AsyncExitStack() as stack:
+            collectors = [await stack.enter_async_context(c) for c in self.collectors]
+            while self.running:
+                for collector in collectors:
+                    data = [value async for value in collector.get_data()]
+                for callback in self.callbacks:
+                    await callback(data)
+                await asyncio.sleep(0.05)
+
+    def stop(self):
+        self.running = False
+
+
+async def main():
+    async def demo_producer(data):
+        print(data)
+
+    stream = SensorStream()
+    phyphox = Phyphox()
+    stream.add_collector(phyphox)
+    stream.add_callback(demo_producer)
+    await stream.start()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
